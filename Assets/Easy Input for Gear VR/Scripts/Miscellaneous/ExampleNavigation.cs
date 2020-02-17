fdbbc55e2c8cc8c4880b6142d9416971d2198f35
LOPEZ Doryan <doryan.lopez@ynov.com>
2020-02-17 09:59:13 +0100

    First commit

---
+++ Assets/Easy Input for Gear VR/Scripts/Miscellaneous/ExampleNavigation.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using EasyInputVR.Core;
+using UnityEngine.SceneManagement;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+namespace EasyInputVR.Misc
+{
+
+    [AddComponentMenu("EasyInputGearVR/Miscellaneous/ExampleNavigation")]
+    public class ExampleNavigation : MonoBehaviour
+    {
+
+        public void button1Submit()
+        {
+            UnitySceneManager.LoadScene("tiltGearVRControllerExample");
+        }
+
+        public void button2Submit()
+        {
+            UnitySceneManager.LoadScene("controlsExample");
+        }
+
+        public void button3Submit()
+        {
+            UnitySceneManager.LoadScene("firstPersonGearVRController");
+        }
+
+        public void button4Submit()
+        {
+            UnitySceneManager.LoadScene("GuiNavigationExample");
+        }
+
+        public void button5Submit()
+        {
+            UnitySceneManager.LoadScene("GamepadControllerDiagnosticExample");
+        }
+
+        public void button6Submit()
+        {
+            UnitySceneManager.LoadScene("GVRcontrollerDiagnosticExample");
+        }
+
+        public void button7Submit()
+        {
+            UnitySceneManager.LoadScene("bowlingExample");
+        }
+
+        public void button8Submit()
+        {
+            UnitySceneManager.LoadScene("pointerExample");
+        }
+
+    }
+}
---
+++ Assets/_Scripts/ObjectApparition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectApparition : MonoBehaviour
+{
+    public float smoothFactor = 2;
+    public GameObject myPrefab;
+    private bool isInstantiated = false;
+    private GameObject player;
+
+    private Vector3 basePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        basePos = new Vector3(0, transform.position.y, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, 2, transform.position.z), Time.deltaTime * smoothFactor);
+        if (transform.position.y >= -1 && gameObject.CompareTag("Photo") && isInstantiated == false)
+        {
+            Instantiate(myPrefab, new Vector3(transform.position.x, 0, transform.position.z), Quaternion.Euler(-90, 0, 0));
+            isInstantiated = true;
+        }
+        Vector3 playerPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+
+        transform.LookAt(playerPosition);
+
+    }
+
+
+}
